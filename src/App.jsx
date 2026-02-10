36d901d153c4693dcc8f50406fcccbc61429006d
Semi <grimzaeye@gmail.com>
2026-02-10 22:36:00 +0900

    fix deploy address

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Location from './pages/Location';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
