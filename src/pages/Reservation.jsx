dd79aa855631ae2dc0547b6a29e17f94576f9a02
Semi <grimzaeye@gmail.com>
2026-02-21 22:44:58 +0900

    feat: update reservation QR code and cleanup old files

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Reservation.module.css';
-import qrCode from '../images/qr_code_reservation.webp';
+import qrCode from '../images/new_qr.webp';
 
 const Reservation = () => {
   return (
